2254eb2026c50c2e1bbfade9a39f1d279b5c6a60
Ak1324 <97510308+Ak1324@users.noreply.github.com>
2024-05-17 18:25:30 +0530

    Create Imagepull-secret.sh

---
+++ Imagepull-secret.sh
@@ -0,0 +1,3 @@
+# Create a image pull secret where the images are stored [Dockerhub for instance]:
+
+kubectl create secret docker-registry regcred --docker-server=<your-registry-server> --docker-username=<your-name> --docker-password=<your-pword> --docker-email=<your-email>
